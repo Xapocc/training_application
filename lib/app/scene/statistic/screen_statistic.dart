4720894896b6235be79f62affb323a736e79ff06
developer <dev@dev>
2021-12-30 10:39:28 +0200

    build 1.0.2+1

--- lib/app/scene/statistic/screen_statistic.dart
+++ lib/app/scene/statistic/screen_statistic.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:training_application/app/scene/statistic/cubit/statistic_cubit.dart';
 import 'package:training_application/app/scene/statistic/cubit/statistic_state.dart';
+import 'package:training_application/app/string.dart';
 import 'package:training_application/domain/statistic/statistic_domain.dart';
 
 class ScreenStatistics extends StatelessWidget {
@@ -12,18 +13,7 @@ class ScreenStatistics extends StatelessWidget {
         super(key: key);
 
   final int _seconds;
-  static const List<String> _images = [
-    "https://unsplash.com/photos/rW-I87aPY5Y/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/gKXKBY-C-Dk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/so5nsYDOdxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Nnx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/mJaD10XeD7w/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NHx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/OzAeZPNsLXk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/NodtnCsLdTE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8OHx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/ZCHj_2lJP00/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8N3x8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
-    "https://unsplash.com/photos/nKC772R_qog/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTF8fGNhdHx8MHx8fHwxNjQwNzA2MDA2",
-    "https://unsplash.com/photos/LEpfefQf4rU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fGNhdHx8MHx8fHwxNjQwNzA2MDA2",
-    "https://unsplash.com/photos/75715CVEJhI/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTV8fGNhdHx8MHx8fHwxNjQwNzA2MDA2",
-  ];
+  static const List<String> _images = AppStrings.imagesCatsUrls;
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +38,7 @@ class ScreenStatistics extends StatelessWidget {
                     height: 32.0,
                   ),
                   Text(
-                    "Please wait ${state.time} seconds",
+                    AppStrings.spinnerText(state.time),
                     style: const TextStyle(
                       fontSize: 24.0,
                       inherit: false,
@@ -60,30 +50,31 @@ class ScreenStatistics extends StatelessWidget {
             );
           } else {
             return FutureBuilder(
-                future: StatisticDomain.getStateCountersMap(),
-                builder: (context, snapshot) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      if (snapshot.hasData)
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            state is DataState
-                                ? "Data state appeared ${(snapshot.data as Map)["dataStateCounter"]} times now"
-                                : "Error state appeared ${(snapshot.data as Map)["errorStateCounter"]} times now",
-                            style: TextStyle(
-                              inherit: false,
-                              color: (state is ErrorState)
-                                  ? Colors.red
-                                  : Colors.green,
-                            ),
+              future: StatisticDomain.getStateCountersMap(),
+              builder: (context, snapshot) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    if (snapshot.hasData)
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          state is DataState
+                              ? "Data state appeared ${(snapshot.data as Map)["dataStateCounter"]} times now"
+                              : "Error state appeared ${(snapshot.data as Map)["errorStateCounter"]} times now",
+                          style: TextStyle(
+                            inherit: false,
+                            color: (state is ErrorState)
+                                ? Colors.red
+                                : Colors.green,
                           ),
                         ),
-                      if (state is DataState) _dataListView(),
-                    ],
-                  );
-                });
+                      ),
+                    if (state is DataState) _dataListView(),
+                  ],
+                );
+              },
+            );
           }
         },
       ),
--- lib/app/string.dart
+++ lib/app/string.dart
@@ -10,4 +10,19 @@ abstract class AppStrings {
   // task 4 screen
   static const String textButtonStartTimerTask4Screen = "Start";
   static const String textHintDefaultTask4Screen = "0";
+
+  // statistic screen
+  static const List<String> imagesCatsUrls = [
+    "https://unsplash.com/photos/rW-I87aPY5Y/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/gKXKBY-C-Dk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/so5nsYDOdxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Nnx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/mJaD10XeD7w/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NHx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/OzAeZPNsLXk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/NodtnCsLdTE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8OHx8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/ZCHj_2lJP00/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8N3x8Y2F0fHwwfHx8fDE2NDA3MDYwMDY",
+    "https://unsplash.com/photos/nKC772R_qog/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTF8fGNhdHx8MHx8fHwxNjQwNzA2MDA2",
+    "https://unsplash.com/photos/LEpfefQf4rU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fGNhdHx8MHx8fHwxNjQwNzA2MDA2",
+    "https://unsplash.com/photos/75715CVEJhI/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTV8fGNhdHx8MHx8fHwxNjQwNzA2MDA2",
+  ];
+  static String spinnerText(time) => "Please wait $time seconds";
 }
