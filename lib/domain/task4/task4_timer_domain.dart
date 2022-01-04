a829a8cf6591b729aded15a34727c69f5cb4e5aa
Alexander Lapin <alexander.lapin@evernetica.com>
2022-01-04 16:51:03 +0200

    refactored repositories

--- lib/domain/task4/task4_timer_domain.dart
+++ lib/domain/task4/task4_timer_domain.dart
@@ -1,11 +1,11 @@
-import 'package:training_application/data/task4/task4_offline_repository.dart';
+import 'package:training_application/data/shared_preferences/shared_preferences_repository.dart';
 
 abstract class Task4TimerDomain {
   static void saveTime(int time) {
-    Task4OfflineRepository.saveTimeToFile(time);
+    SharedPreferencesRepository.saveTimeToFile(time);
   }
 
   static Future<int> getTime() {
-    return Task4OfflineRepository.getTimeFromFile();
+    return SharedPreferencesRepository.getTimeFromFile();
   }
 }
