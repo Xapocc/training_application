e96f9880d6bc3290f2e051f57f32108fd5b5b1c2
Alexander Lapin <alexander.lapin@evernetica.com>
2022-02-14 17:56:33 +0200

    change: code cleanup

--- lib/app/scene/task4/cubit/task4_cubit.dart
+++ lib/app/scene/task4/cubit/task4_cubit.dart
@@ -1,28 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:training_application/app/scene/task4/cubit/task4_state.dart';
 import 'package:training_application/app/size.dart';
-import 'package:training_application/app/string.dart';
-import 'package:training_application/domain/entities/statistic_entity.dart';
-import 'package:training_application/domain/entities/statistic_last_date_entity.dart';
 import 'package:training_application/domain/entities/task4_entity.dart';
 import 'package:training_application/main.dart';
 
 class Task4ScreenCubit extends Cubit<Task4ScreenState> {
   late StreamController _controllerTime;
-  late StreamController _controllerCounter;
-  late StreamController _controllerLastDate;
 
   StreamSubscription? _subscriptionTime;
-  StreamSubscription? _subscriptionCounter;
-  StreamSubscription? _subscriptionLastDate;
 
-  Task4ScreenCubit() : super(Task4ScreenState(isButtonEnabled: false)) {
+  Task4ScreenCubit() : super(const Task4ScreenState(isButtonEnabled: false)) {
     _controllerTime = StreamController<Task4Entity>();
-    _controllerCounter = StreamController<StatisticEntity>();
-    _controllerLastDate = StreamController<StatisticLastDateEntity>();
 
     _subscriptionTime = _controllerTime.stream.listen((event) {
       emit(Task4ScreenState.fromState(state,
@@ -32,38 +22,7 @@ class Task4ScreenCubit extends Cubit<Task4ScreenState> {
       _subscriptionTime?.cancel();
     });
 
-    _subscriptionCounter = _controllerCounter.stream.listen((event) {
-      emit(Task4ScreenState.fromState(state,
-          dataStateCounter: (event as StatisticEntity).dataStateCounter,
-          errorStateCounter: event.errorStateCounter));
-
-      _subscriptionCounter?.cancel();
-    });
-
-    _subscriptionLastDate = _controllerLastDate.stream.listen((event) {
-      DateTime lastDataDate = DateTime.fromMillisecondsSinceEpoch(
-          (event as StatisticLastDateEntity).dataStateLastDate);
-      DateTime lastErrorDate =
-          DateTime.fromMillisecondsSinceEpoch(event.errorStateLastDate);
-
-      emit(Task4ScreenState.fromState(
-        state,
-        dataStateLastDate:
-            event.dataStateLastDate != AppSizes.lastDateDefaultMilliseconds
-                ? DateFormat(AppStrings.formatLastDate).format(lastDataDate)
-                : l10n.lastDateDefault,
-        errorStateLastDate:
-            event.errorStateLastDate != AppSizes.lastDateDefaultMilliseconds
-                ? DateFormat(AppStrings.formatLastDate).format(lastErrorDate)
-                : l10n.lastDateDefault,
-      ));
-
-      _subscriptionLastDate?.cancel();
-    });
-
     getTime();
-    getStateCountersMap();
-    getStateLastDateMap();
   }
 
   void saveTime(int time) {
@@ -73,16 +32,6 @@ class Task4ScreenCubit extends Cubit<Task4ScreenState> {
   Future<void> getTime() async {
     _controllerTime
         .add(await task4TimerUseCase!.getTime(auth?.currentUser?.uid ?? ""));
-  }
-
-  Future<void> getStateCountersMap() async {
-    _controllerCounter.add(await statisticUseCase!
-        .getStateCountersMap(auth?.currentUser?.uid ?? ""));
-  }
-
-  Future<void> getStateLastDateMap() async {
-    _controllerLastDate.add(await statisticLastDateUseCase!
-        .getStateLastDatesMap(auth?.currentUser?.uid ?? ""));
   }
 
   void checkIfSecondsInRange(int seconds) {
--- lib/app/scene/task4/cubit/task4_state.dart
+++ lib/app/scene/task4/cubit/task4_state.dart
@@ -1,48 +1,28 @@
 import 'package:equatable/equatable.dart';
-import 'package:training_application/main.dart';
 
 class Task4ScreenState extends Equatable {
-  Task4ScreenState(
-      {bool isButtonEnabled = false,
-      int seconds = 0,
-      int secondsStart = 0,
-      int dataStateCounter = 0,
-      int errorStateCounter = 0,
-      String? dataStateLastDate,
-      String? errorStateLastDate})
-      : _isButtonEnabled = isButtonEnabled,
+  const Task4ScreenState({
+    bool isButtonEnabled = false,
+    int seconds = 0,
+    int secondsStart = 0,
+  })  : _isButtonEnabled = isButtonEnabled,
         _seconds = seconds,
         _secondsStart = secondsStart,
-        _dataStateCounter = dataStateCounter,
-        _errorStateCounter = errorStateCounter,
-        _dataStateLastDate = dataStateLastDate ?? l10n.lastDateDefault,
-        _errorStateLastDate = errorStateLastDate ?? l10n.lastDateDefault,
         super();
 
-  Task4ScreenState.fromState(Task4ScreenState state,
-      {bool? isButtonEnabled,
-      int? seconds,
-      int? secondsStart,
-      int? dataStateCounter,
-      int? errorStateCounter,
-      String? dataStateLastDate,
-      String? errorStateLastDate})
-      : _isButtonEnabled = isButtonEnabled ?? state.isButtonEnabled,
+  Task4ScreenState.fromState(
+    Task4ScreenState state, {
+    bool? isButtonEnabled,
+    int? seconds,
+    int? secondsStart,
+  })  : _isButtonEnabled = isButtonEnabled ?? state.isButtonEnabled,
         _seconds = seconds ?? state.seconds,
         _secondsStart = secondsStart ?? state.secondsStart,
-        _dataStateCounter = dataStateCounter ?? state.dataStateCounter,
-        _errorStateCounter = errorStateCounter ?? state.errorStateCounter,
-        _dataStateLastDate = dataStateLastDate ?? state.dataStateLastDate,
-        _errorStateLastDate = errorStateLastDate ?? state.errorStateLastDate,
         super();
 
   final bool _isButtonEnabled;
   final int _seconds;
   final int _secondsStart;
-  final int _dataStateCounter;
-  final int _errorStateCounter;
-  final String _dataStateLastDate;
-  final String _errorStateLastDate;
 
   bool get isButtonEnabled => _isButtonEnabled;
 
@@ -50,22 +30,10 @@ class Task4ScreenState extends Equatable {
 
   int get secondsStart => _secondsStart;
 
-  int get dataStateCounter => _dataStateCounter;
-
-  int get errorStateCounter => _errorStateCounter;
-
-  String get dataStateLastDate => _dataStateLastDate;
-
-  String get errorStateLastDate => _errorStateLastDate;
-
   @override
   List<Object?> get props => [
         _isButtonEnabled,
         _seconds,
         _secondsStart,
-        _dataStateCounter,
-        _errorStateCounter,
-        _dataStateLastDate,
-        _errorStateLastDate
       ];
 }
